2291a1de304d01389570e925dd8ef23ceb8fefdc
zhuang <zhuang@example.com>
2022-10-06 14:04:17 +0800

    first

---
+++ src/main/java/com/projectname/Demo/Calc.java
@@ -0,0 +1,17 @@
+package com.projectname.Demo;
+
+import java.awt.geom.Area;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author 86138
+ */
+public class Calc {
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static int subtract(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/com/projectname/Demo/TestCalc.java
@@ -0,0 +1,42 @@
+package com.projectname.Demo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.awt.geom.Area;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestCalc
+        extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TestCalc( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( TestCalc.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testCalc()
+    {
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4,2));
+    }
+
+}
